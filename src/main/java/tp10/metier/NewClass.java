b1d99c45e952edda5cf9e2ce709c774d0e454c73
lenovo <lenovo@LAPTOP-0Q4N83SA>
2022-03-11 14:26:04 +0100

    Update xxx

--- src/main/java/tp10/metier/NewClass.java
+++ src/main/java/tp10/metier/NewClass.java
@@ -20,42 +20,8 @@ package tp10.metier;
 
 /**
  *
- * @author perussel
+ * @author lenovo
  */
-public abstract class Action {
-
-    private String nom;
-
-    /**
-     * Get the value of nom
-     *
-     * @return the value of nom
-     */
-    public String getNom() {
-        return nom;
-    }
-
-    public Action(String nom) {
-        this.nom = nom;
-    }
-
-    public abstract float valeur(Jour j);
-
-    @Override
-    public boolean equals(Object obj) {
-        Action actionAComparer;
-        
-        actionAComparer = (Action)obj;
-       return (this.getNom().compareToIgnoreCase(actionAComparer.getNom()) == 0);
-    }
-
-    @Override
-    public int hashCode() {
-        return this.getNom().toUpperCase().hashCode();
-    }
+public class NewClass {
     
-    @Override
-    public String toString() {
-        return this.getNom();
-    }
 }
